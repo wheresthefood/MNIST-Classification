45e6480768436b8140bb03f6cd3228a6d0ea6e16
zswarth <zachary.swarth@gmail.com>
2018-03-19 23:14:35 -0700

    mnist

--- MultiLayerFinal.py
+++ MultiLayerFinal.py
@@ -1,6 +1,5 @@
 import numpy as np
 import random
-from scipy.special import expit
 
 
 #only for binary distinction
@@ -75,22 +74,4 @@ class NN(object):
  		correct = pr(out2)==labels
 		return correct*1, pr(out2), out2
 
-# data = np.loadtxt('sin.csv', delimiter = ',')
-# x = data[:, 0:2]
-# y = data[:, 2:3]
 
-
-# a = NN(x, y)
-
-# c,d = a.feed_foward(x, a.wh, a.wo, a.bh, a.bo)
-
-# w1,w2, bh, bo, error = a.train(x, y,a.wh, a.wo, a.bh, a.bo, alpha = .0005, iter = 200)
-
-
-# c,d,e   = a.test(x, y, w1, w2, bh, bo)
-# # print len(c)
-# print sum(c)
-# print sum(d)
-
-
-# data = np.loadtxt('sin.csv', delimiter = ',')
